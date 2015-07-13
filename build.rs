8699d6c2e19720269be36d0ae92757de1da32047
Michael Neumann <mneumann@ntecs.de>
2015-07-13 18:47:36 +0200

    Simplify building of libsophia.a

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
+use std::process::Command;
+
 fn main() {
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
+    let mut cmd = Command::new("make");
+    cmd.arg("static").current_dir("sophia");
+    cmd.status().unwrap();
+    println!("cargo:rustc-link-search=native=./sophia");
     println!("cargo:rustc-link-lib=static=sophia");
 }
