9db37ee66649f4dbf2944b25ddae7e64057b33db
Michael Neumann <mneumann@ntecs.de>
2015-07-13 19:10:16 +0200

    Copy libsophia.a into OUT_DIR

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
 use std::process::Command;
+use std::env;
+use std::fs;
+use std::path::Path;
 
 fn main() {
     let mut cmd = Command::new("make");
     cmd.arg("static").current_dir("sophia");
     cmd.status().unwrap();
-    println!("cargo:rustc-link-search=native=./sophia");
+
+    let dst = env::var("OUT_DIR").unwrap();
+    fs::copy("sophia/libsophia.a", Path::new(&dst).join("libsophia.a")).unwrap();
+
+    println!("cargo:rustc-link-search=native={}", dst);
     println!("cargo:rustc-link-lib=static=sophia");
 }
